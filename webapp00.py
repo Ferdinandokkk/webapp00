974d1f3a07e281eda0bc3113bc28ea1b5c9c1238
Fernando Silva <93625097+Ferdinandokkk@users.noreply.github.com>
2022-09-19 10:38:35 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -15,8 +15,9 @@ st.subheader("Rústico")
 # Use st.write("") para adicionar um texto ao seu Web app
 st.write("VAMO PRA CIMA PORCOOOOO")
 
-col1, col2, col3 = st.columns(3)
-col1.metric("Temperature", "19 °C", "-15 °C")
-col2.metric("Wind", "9 mph", "-8%")
-col3.metric("Humidity", "86%", "4%")
+col1, col2, col3 = st.columns(4)
+col1.metric("Temperatura", "19 °C", "-15 °C")
+col2.metric("Sensação Térmica", "20°C", "-2 °C")
+col3.metric("Vento", "30 kmh", "-8%")
+col4.metric("Humidade", "86%", "4%")
 
